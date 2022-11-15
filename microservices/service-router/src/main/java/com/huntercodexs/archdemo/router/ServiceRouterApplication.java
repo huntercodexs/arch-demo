8ae6d38732154d83d40b71d16910352c793246fb
Jereelton Teixeira <jereeltondevel@gmail.com>
2022-11-15 15:12:10 -0300

    update: SERVICE-ROUTER with @Controller

--- microservices/service-router/src/main/java/com/huntercodexs/archdemo/router/ServiceRouterApplication.java
+++ microservices/service-router/src/main/java/com/huntercodexs/archdemo/router/ServiceRouterApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
+@Controller
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ServiceRouterApplication {
